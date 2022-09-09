6a574b94f625b89a2edf388ce97c05ca4ed6833b
LOFT <Patch97236@gmail.com>
2022-09-08 20:12:23 -0700

    Validation for new customer

--- DemoProducts/Models/CustomerDTO.cs
+++ DemoProducts/Models/CustomerDTO.cs
@@ -5,22 +5,23 @@ namespace DemoProducts.Data;
 public class CustomerDto
 {
     public int Id { get; set; }
-    public string CompanyName { get; set; } = "";
-    [Required(ErrorMessage ="Please enter company name..")]
-    public string ContactName { get; set; } = "";
-    [Required(ErrorMessage ="Please enter contact name..")]
-    public string PhoneNumber { get; set; } = "";
-    [Required(ErrorMessage ="Please enter a phone number..")]
-    public string EmailAddress { get; set; } = "";
     
-    public string Street { get; set; } = "";
+    [Required(ErrorMessage ="Please enter company name..")]
+    public string CompanyName { get; set; } = "";
+    [Required(ErrorMessage ="Please enter contact name..")]
+    public string ContactName { get; set; } = "";
+    [Required(ErrorMessage ="Please enter a phone number..")]
+    public string PhoneNumber { get; set; } = "";
+    public string EmailAddress { get; set; } = "";
     [Required(ErrorMessage ="Please enter a street address..")]
-    public string City { get; set; } = "";
+    public string Street { get; set; } = "";
     [Required(ErrorMessage ="Please enter city..")]
-    public string State { get; set; } = "";
+    public string City { get; set; } = "";
     [Required(ErrorMessage ="Please enter state..")]
-    public string ZipCode { get; set; } = "";
+    public string State { get; set; } = "";
     [Required(ErrorMessage ="Please enter zip code..")]
+    public string ZipCode { get; set; } = "";
+
 
     public string Notes { get; set; } = "";
 }
